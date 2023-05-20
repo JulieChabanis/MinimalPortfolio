a8b6b0016cdf0265c38ea65162a86d204184b66a
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-05-20 15:21:57 +0200

    init theming + add MUI Library

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import { ColorModeContext, useMode } from './UI/Theme/theme'; 
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { motion } from "framer-motion"
 import './App.css';
 
 function App() {
+  const [theme, colorMode] = useMode(); 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        Contenu animé avec Framer Motion
-      </motion.div>
-    </div>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+          <div className="App">
+            <header className="App-header">
+            <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+              Julie Chabanis
+            </motion.div>
+          </div>
+            </header>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/UI/Theme/theme.js
@@ -0,0 +1,98 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material";
+
+// color design tokens
+export const tokens = (mode) => ({
+  ...(mode === "dark"
+  ? {
+    grey: {
+      100: "#B799FF",
+      200: "#B799FF", 
+      300: "#B799FF",
+      400: "#B799FF",
+      500: "#B799FF",
+    }, 
+    primary: {
+      100: "#ACBCFF",
+      200: "#ACBCFF", 
+      300: "#ACBCFF",
+      400: "#ACBCFF",
+      500: "#ACBCFF",
+    },
+  }
+  : {
+    grey: {
+      100: "#C4DFDF",
+      200: "#C4DFDF", 
+      300: "#C4DFDF",
+      400: "#C4DFDF",
+      500: "#C4DFDF",
+    }, 
+    primary: {
+      100: "#F266AB",
+      200: "#F266AB", 
+      300: "#F266AB",
+      400: "#F266AB",
+      500: "#F266AB",
+    },
+
+  }),
+}); 
+
+// theme settings
+export const themeSettings = (mode) => {
+  const colors = tokens(mode); 
+
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+      ? {
+
+        // dark mode
+        primary: {
+          main: colors.primary[500],
+        }, 
+        secondary: {
+          main: colors.grey[500],
+        }, 
+        background: {
+          default: "#000000", 
+        }, 
+      } : {
+        // light mode
+        primary: {
+          main: colors.primary[500],
+        }, 
+        secondary: {
+          main: colors.grey[500],
+        }, 
+        background: {
+          default: "#f2f2f2"
+        }, 
+      }),
+    },
+  };
+};
+
+// context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {}
+}); 
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark"); 
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => 
+      setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  ); 
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); 
+
+  return [theme, colorMode]; 
+
+}
