e962b2d8e059b08f7181749767a6b1c04cf8a328
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-05-23 19:08:19 +0200

    Typing animation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import { ColorModeContext, useMode } from './UI/Theme/theme'; 
+import { ColorModeContext, useMode } from './Theme/theme'; 
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { motion } from "framer-motion"
+import { motion, useCycle } from "framer-motion"
 import './App.css';
 
 function App() {
-  const [theme, colorMode] = useMode(); 
+  const [theme, colorMode] = useMode();
 
+  const TextAnimation = () => {
+    // eslint-disable-next-line no-unused-vars
+    const [isTyping, setIsTyping] = useCycle(true, false);
+
+    return (
+      <div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          style={theme.typography.h1}
+        >
+          Julie Chabanis
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          style={theme.typography.h2}
+        >
+          {isTyping ? "Develop|" : "Developper"}
+        </motion.div>
+      </div>
+    );
+  };
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline/>
-          <div className="App">
-            <header className="App-header">
-            <div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-              style={theme.typography.h1}
-            >
-              Julie Chabanis
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-              style={theme.typography.h2}
-            >
-              Developper
-            </motion.div>
-          </div>
-            </header>
-          </div>
+        <div className="App">
+          <header className="App-header">
+            <TextAnimation />
+          </header>
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
