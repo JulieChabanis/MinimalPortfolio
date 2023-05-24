8d34e549541dbe011cf6f9943b22f450f2782b54
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-05-24 15:55:42 +0200

    Add animation text on HeroPage

--- src/components/HeroPage/HeroPage.jsx
+++ src/components/HeroPage/HeroPage.jsx
@@ -1,29 +1,47 @@
 import { Box, useTheme } from "@mui/material";
-import { motion, useCycle} from "framer-motion";
-import React from 'react'
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const HeroPage = () => {
   const theme = useTheme();
-  const [isTyping, setIsTyping] = useCycle(true, false); 
-  
+
+  const words = [
+    { word: 'HELLO', translation: 'BONJOUR' },
+    { word: 'Bonjour', translation: 'Bonjour en français'},
+    { word: 'こんにちは', translation: 'Bonjour en japonais' },
+    { word: 'Góðan dag', translation: 'Bonjour en islandais' },
+  ];
+
+  const [currentLanguageIndex, setCurrentLanguageIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentLanguageIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [words.length]);
+
   return (
     <Box>
+    <motion.div
+        key={currentLanguageIndex} // Ajouter la clé pour déclencher une nouvelle animation
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        style={theme.typography.h2}
+        transition={{ duration: 0.5 }} // Ajouter une durée de transition
+      >
+        {words[currentLanguageIndex].word}
+      </motion.div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
         style={theme.typography.h1}
       >
-          Julie Chabanis
+        Julie Chabanis
       </motion.div>
-      <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-          style={theme.typography.h2}
-        >
-          {isTyping ? "Develop|" : "Developper"}
-        </motion.div>
     </Box>
   );
 };
