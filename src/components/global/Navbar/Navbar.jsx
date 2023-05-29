57439e85ada70910ec3f067977873292825dfd49
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-05-29 13:11:34 +0200

    update heropage section with logo

--- src/components/global/Navbar/Navbar.jsx
+++ src/components/global/Navbar/Navbar.jsx
@@ -10,29 +10,34 @@ const NavBar = () => {
   // const colors = tokens(theme.palette.mode); 
 
   return (
-    <nav className={styles.navBar}>
-      <Box className={styles.nav}>
-        <List className={styles.navList} style={theme.typography.body1}>
-          <ListItem>
-            <ListItemText style={theme.typography.h6}>Skills</ListItemText>
-          </ListItem>
-          <ListItem>
-          <ListItemText style={theme.typography.h6}>Projects</ListItemText>
-          </ListItem>
-          <ListItem>
-          <ListItemText style={theme.typography.h6}>Portfolio</ListItemText>
-          </ListItem>
-          <ListItem>
-          <ListItemText style={theme.typography.h6}>Contact</ListItemText>
-          </ListItem>
-          <ListItem>
-          <IconButton className={styles.navIcons}>
-            <GitHubIcon/>
-          </IconButton>
-          </ListItem>
-        </List>
+    <Box>
+      <Box className={styles.logo}>
+      JC
       </Box>
-    </nav>
+      <nav className={styles.navBar}>
+        <Box className={styles.nav}> 
+          <List className={styles.navList}>
+          <ListItem>
+              <ListItemText style={theme.typography.h6}>Skills</ListItemText>
+            </ListItem>
+            <ListItem>
+            <ListItemText style={theme.typography.h6}>Projects</ListItemText>
+            </ListItem>
+            <ListItem>
+            <ListItemText style={theme.typography.h6}>Portfolio</ListItemText>
+            </ListItem>
+            <ListItem>
+            <ListItemText style={theme.typography.h6}>Contact</ListItemText>
+            </ListItem>
+            <ListItem>
+            <IconButton className={styles.navIcons}>
+              <GitHubIcon/>
+            </IconButton>
+            </ListItem>
+          </List>
+        </Box>
+      </nav>
+    </Box>
 
   )
 }
