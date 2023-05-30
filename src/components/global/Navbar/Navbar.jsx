f4376b26067eed200415e8a24657059a44f7a04c
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-05-30 17:50:15 +0200

    update navbar effects

--- src/components/global/Navbar/Navbar.jsx
+++ src/components/global/Navbar/Navbar.jsx
@@ -2,47 +2,77 @@ import styles from "./Navbar.module.css";
 import { Box, List, ListItem, ListItemText, useTheme, IconButton } from "@mui/material";
 import { Link } from "react-scroll"; 
 import GitHubIcon from "@mui/icons-material/GitHub";
+import { motion } from "framer-motion";
 import React from "react";
 
 const NavBar = () => {
   const theme = useTheme();
   
-
   return (
-    <Box className={styles.navBar}>
+    <motion.nav
+      className={styles.navBar}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
       <Box className={styles.logo}>
-      <Link to="heropage-section" smooth={true} duration={500} className={styles.navLink}>
-        JC
-      </Link>
+        <Link to="heropage-section" smooth={true} duration={200} className={styles.navLink}>
+          JC
+        </Link>
       </Box>
       <List className={styles.navList}>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <ListItem>
-              <Link to="work-section" smooth={true} duration={500} className={styles.navLink}>
-                <ListItemText style={theme.typography.h6}>Experience</ListItemText>
-              </Link>
-            </ListItem>
-        <ListItem>
-          <Link to="skills-section" smooth={true} duration={500}>
-            <ListItemText style={theme.typography.h6}>Skills</ListItemText>
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link to="portfolio" smooth={true} duration={500}>
-            <ListItemText style={theme.typography.h6}>Portfolio</ListItemText>
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link to="contact" smooth={true} duration={500}>
-            <ListItemText style={theme.typography.h6}>Contact</ListItemText>
-          </Link>
-        </ListItem>
-        <ListItem>
-          <IconButton className={styles.navIcons}>
-            <GitHubIcon />
-          </IconButton>
-        </ListItem>
+            <Link to="work-section" smooth={true} duration={200} className={styles.navLink}>
+              <ListItemText style={theme.typography.h6}>Experience</ListItemText>
+            </Link>
+          </ListItem>
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <ListItem>
+            <Link to="skills-section" smooth={true} duration={200} className={styles.navLink}>
+              <ListItemText style={theme.typography.h6}>Skills</ListItemText>
+            </Link>
+          </ListItem>
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <ListItem>
+            <Link to="portfolio" smooth={true} duration={200} className={styles.navLink}>
+              <ListItemText style={theme.typography.h6}>Portfolio</ListItemText>
+            </Link>
+          </ListItem>
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <ListItem>
+            <Link to="contact" smooth={true} duration={200} className={styles.navLink}>
+              <ListItemText style={theme.typography.h6}>Contact</ListItemText>
+            </Link>
+          </ListItem>
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <ListItem>
+            <IconButton className={styles.navIcons}>
+              <GitHubIcon />
+            </IconButton>
+          </ListItem>
+        </motion.div>
       </List>
-    </Box>
+    </motion.nav>
   );
 };
 
