8deae15d3b743a6274ee319c84391b08b6600019
JulieChabanis <98469515+JulieChabanis@users.noreply.github.com>
2023-06-19 14:33:52 +0200

    Swipeable Menu WIP

--- src/components/global/Navbar/SwipeableDrawerMenu.jsx
+++ src/components/global/Navbar/SwipeableDrawerMenu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { MenuItem, SwipeableDrawer, ListItemText, IconButton } from "@mui/material";
+import { ListItem, SwipeableDrawer, ListItemText, Box, IconButton } from "@mui/material";
 import { Link } from "react-scroll";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import styles from "./SwipeableDrawerMenu.module.css";
 import { useTheme } from "@mui/material/styles";
+import CloseIcon from "@mui/icons-material/Close";
 
 const SwipeableDrawerMenu = ({ menuOpen, handleMenuClose }) => {
   const theme = useTheme();
@@ -18,45 +19,50 @@ const SwipeableDrawerMenu = ({ menuOpen, handleMenuClose }) => {
       onClose={handleMenuClose}
       PaperProps={{ style: { width: 150, height: 350 } }}
     >
-      <MenuItem>
-        <Link to="heropage-section" smooth={true} duration={200} className={styles.navLink}>
+      <Box>
+        <IconButton className={styles.closeButton} onClick={handleMenuClose}>
+          <CloseIcon />
+        </IconButton>
+      </Box>   
+      <ListItem>
+        <Link to="heropage-section" smooth={true} duration={200} className={styles.navSwipeableLink}>
           <ListItemText style={theme.typography.h6}>Home</ListItemText>
         </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link to="work-section" smooth={true} duration={200} className={styles.navLink}>
+      </ListItem>
+      <ListItem>
+        <Link to="work-section" smooth={true} duration={200} className={styles.navSwipeableLink}>
           <ListItemText style={theme.typography.h6}>Experience</ListItemText>
         </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link to="skills-section" smooth={true} duration={200} className={styles.navLink}>
+      </ListItem>
+      <ListItem>
+        <Link to="skills-section" smooth={true} duration={200} className={styles.navSwipeableLink}>
           <ListItemText style={theme.typography.h6}>Skills</ListItemText>
         </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link to="portfolio" smooth={true} duration={200} className={styles.navLink}>
+      </ListItem>
+      <ListItem>
+        <Link to="portfolio" smooth={true} duration={200} className={styles.navSwipeableLink}>
           <ListItemText style={theme.typography.h6}>Portfolio</ListItemText>
         </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link to="contact" smooth={true} duration={200} className={styles.navLink}>
+      </ListItem>
+      <ListItem>
+        <Link to="contact" smooth={true} duration={200} className={styles.navSwipeableLink}>
           <ListItemText style={theme.typography.h6}>Contact</ListItemText>
         </Link>
-      </MenuItem>
-      <MenuItem>
+      </ListItem>
+      <ListItem>
         <a href="https://github.com/JulieChabanis" target="_blank" rel="noopener noreferrer">
           <IconButton className={styles.navSwipeableIcons}>
             <GitHubIcon />
           </IconButton>
         </a>
-      </MenuItem>
-      <MenuItem>
+      </ListItem>
+      <ListItem>
         <a href="https://www.linkedin.com/in/julie-chabanis-80695690/" target="_blank" rel="noopener noreferrer">
           <IconButton className={styles.navSwipeableIcons}>
             <LinkedInIcon />
           </IconButton>
         </a>
-      </MenuItem>
+      </ListItem>
     </SwipeableDrawer>
   );
 };
