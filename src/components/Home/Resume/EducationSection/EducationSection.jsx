2f9277c23f2a27578b40ff1305e8c00838490216
JulieChabanis <julie.chabanis07@gmail.com>
2023-06-26 20:58:31 +0200

    update xs breackpoint grid

--- src/components/Home/Resume/EducationSection/EducationSection.jsx
+++ src/components/Home/Resume/EducationSection/EducationSection.jsx
@@ -12,33 +12,26 @@ const EducationSection = () => {
 
   return (
     <Box>
-      <List>
 
         {/* Education 0 */}
-        <ListItem>
           <Grid 
             container 
             spacing={{ xs: 0, sm: 2, md: 2, lg: 2 }} 
             alignItems="center"
           >
-            <Grid item xs={1} sm={1} md={1} lg={1}>
-              <IconButton onClick={() => handleToggle(0)}>
-                {expanded === 0 ? <ExpandLess /> : <ExpandMore />}
-              </IconButton>
-            </Grid>
-            <Grid item xs={11} sm={5} md={5} lg={5}>
-              <Typography variant="h10"> Bachelor Digital Solutions Developer</Typography>
-            </Grid>
-            <Grid item xs={11} sm={5} md={4} lg={5}>
-              <Typography marginLeft={{ xs: 4, sm: 0, md: 0, lg: 0}} variant="h4">HETIC, Montpellier, FR</Typography>
-            </Grid>
-            <Grid item xs={1} sm={1} md={1} lg={1}>
-              <Typography variant="h10"> 
-                {"2023"}
-              </Typography>
-            </Grid>
+              <Grid item xs={12} sm={1} md={1} lg={1} marginLeft={{ xs: -1.5, sm: 0, md: 0, lg: 0}} marginBottom={{ xs: -1 }}>
+                <IconButton onClick={() => handleToggle(0)}>
+                  {expanded === 0 ? <ExpandLess /> : <ExpandMore />}
+                </IconButton>
+                <Typography variant="h10"> Bachelor Digital Solutions Developer</Typography>
+              </Grid>
+              <Grid item xs={10} sm={5} md={4} lg={5} paddingBottom={2}>
+                <Typography marginLeft={{ xs: 4, sm: 0, md: 0, lg: 0}} variant="h4">EJCAM, Aix-en-Provence, FR</Typography>
+              </Grid>
+              <Grid item xs={2} sm={1} md={1} lg={1} paddingBottom={2}>
+                <Typography marginLeft={{ xs: 2, sm: 0, md: 0, lg: 0}} variant="h10">2018</Typography>
+              </Grid>
           </Grid>
-        </ListItem>
 
         <Collapse in={expanded === 0} timeout="auto" unmountOnExit>
               <Typography variant="h6" className={styles.collapsedText}>
@@ -46,7 +39,7 @@ const EducationSection = () => {
               </Typography>
         </Collapse>
 
-        <Divider />
+        <Divider spacing={2}/>
 
         {/* Education 1 */}
         <ListItem>
@@ -60,11 +53,11 @@ const EducationSection = () => {
                 {expanded === 1 ? <ExpandLess /> : <ExpandMore />}
               </IconButton>
             </Grid>
-            <Grid item xs={11} sm={5} md={5} lg={5}>
+            <Grid item xs={12} sm={5} md={5} lg={5}>
               <Typography variant="h10">Master Communication & Marketing</Typography>
             </Grid>
             <Grid item xs={11} sm={5} md={4} lg={5}>
-              <Typography  marginLeft={{ xs: 4, sm: 0, md: 0, lg: 0}} variant="h4">EJCAM, Aix-en-Provence, FR</Typography>
+              <Typography  marginLeft={{ xs: 5, sm: 0, md: 0, lg: 0}} variant="h4">EJCAM, Aix-en-Provence, FR</Typography>
             </Grid>
             <Grid item xs={1} sm={1} md={1} lg={1}>
               <Typography variant="h10">2018</Typography>
@@ -91,8 +84,9 @@ const EducationSection = () => {
             <Grid item xs={11} sm={5} md={5} lg={5}>
               <Typography variant="h10">Licence Communication & Media</Typography>
             </Grid>
-            <Grid item xs={11} sm={5} md={4} lg={5}>
+            <Grid item xs={12} sm={5} md={4} lg={5}>
               <Typography  marginLeft={{ xs: 4, sm: 0, md: 0, lg: 0}} variant="h4">EJCAM, Aix-en-Provence, FR</Typography>
+              <Typography variant="h10"> {"2023"}</Typography>
             </Grid>
             <Grid item xs={1} sm={1} md={1} lg={1}>
               <Typography variant="h10">2016</Typography>
@@ -105,8 +99,6 @@ const EducationSection = () => {
             {"Media studies • Influence strategy • New Media"}
           </Typography>
         </Collapse>
-        
-      </List>
     </Box>
   );
 }
