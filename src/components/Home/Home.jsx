7f77b07136492d5db225e78ac3be885c425297a2
JulieChabanis <julie.chabanis07@gmail.com>
2023-07-01 15:03:46 +0200

    add Contact section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,21 +6,22 @@ import HeroPage from "./HeroPage/HeroPage";
 import About from "./About/About";
 import Resume from "./Resume/Resume";
 import Portfolio from "./Portfolio/Portfolio";
+import Contact from "./Contact/Contact";
 
 const Home = () => {
   const heropageRef = useRef(); 
   const aboutRef = useRef(); 
   const ResumeRef = useRef();
   const PortfolioRef = useRef(); 
+  const ContactRef = useRef(); 
 
   return (
     <Box className={styles.mainHome}>
-      <HeroPage ref={heropageRef}
-      />
+      <HeroPage ref={heropageRef}/>
       <About ref={aboutRef} />
       <Resume ref={ResumeRef} />
-      <Portfolio ref={PortfolioRef}
-      />
+      <Portfolio ref={PortfolioRef}/>
+      <Contact ref={ContactRef}/>
     </Box>
   )
 }
