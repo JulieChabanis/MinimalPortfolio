0c2e6e5299d7f7998b37db19af6ffeadf9f779a2
JulieChabanis <julie.chabanis07@gmail.com>
2023-07-04 13:53:41 +0200

    update size font

--- src/components/Theme/theme.js
+++ src/components/Theme/theme.js
@@ -175,10 +175,10 @@ export const themeSettings = (mode) => {
       h6: {
         fontFamily: ["Martian Mono", "sans-serif"].join(","), 
         "@media (max-width: 600px)": {
-          fontSize: "0.8rem", 
+          fontSize: "0.7rem", 
         },
         "@media (min-width: 601px)": {
-          fontSize: "0.8rem", 
+          fontSize: "0.7rem", 
         },
         fontWeight: 200,
         lineHeight: 2, 
