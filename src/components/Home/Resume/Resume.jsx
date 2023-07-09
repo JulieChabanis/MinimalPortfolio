6a10584342b3d3e7f6c3cd253be5912aab590e47
JulieChabanis <julie.chabanis07@gmail.com>
2023-07-09 16:52:06 +0200

    add Skills SECTION

--- src/components/Home/Resume/Resume.jsx
+++ src/components/Home/Resume/Resume.jsx
@@ -4,6 +4,7 @@ import { Box, useTheme, Grid, Typography } from '@mui/material';
 import styles from './Resume.module.css';
 import ResumeData from './ResumeData';
 import CardResume from './CardResume.jsx/CardResume';
+import SkillsSection from './Skills/SkillsSection';
 
 const Resume = forwardRef(() => {
   const theme = useTheme();
@@ -16,49 +17,22 @@ const Resume = forwardRef(() => {
           <Box className={styles.titleSection}>{'Resume'}</Box>
         </Box>
 
+        {/*EDUCATION & WORK TIMELINE RESUME*/}
         <Box>
-        <Grid container spacing={6}>
+          <Grid container spacing={6}>
 
-          {/* EDUCATION SECTION */}
-          <Grid item xs={12} sm={12} md={5} lg={4} xl={3} marginTop={-3}>
-          <Typography className={styles.sectionEducationText} variant="h21">{"Education"}</Typography>
-            <Box className={styles.educationSection}>
-              <Box 
-                className={styles.resumeTimeline}
-              >
-                {ResumeData.map((element, id) => {
-                  if (element.category === 'education') {
-                    return (
-                      <CardResume
-                        key={id}
-                        title={element.title}
-                        year={element.year}
-                        society={element.society}
-                        desc={element.desc}
-                      />
-                    );
-                  }
-                  return null; // Added a return statement for non-matching elements
-                })}
-              </Box>
-            </Box>
-          </Grid>
-
-          {/* WORK TIMELINE SECTION */}
-          <Grid item xs={12} sm={12} md={6} lg={6} xl={6.5}>
-            <Box className={styles.WorkSection}>
-              <Typography className={styles.sectionWorkText} variant="h21">{"Work Timeline"}</Typography>
+            {/* EDUCATION SECTION */}
+            <Grid item xs={12} sm={12} md={5} lg={4} xl={3} marginTop={-3}>
+            <Typography className={styles.sectionEducationText} variant="h21">{"Education"}</Typography>
+              <Box className={styles.educationSection}>
                 <Box 
                   className={styles.resumeTimeline}
-                  sx={{
-                    backgroundColor: theme.palette.mode === 'dark' ? '#d8e5e2' : '#eee3ff',
-                  }}
                 >
-                  {ResumeData.map((element, index) => {
-                    if (element.category === 'experience') {
+                  {ResumeData.map((element, id) => {
+                    if (element.category === 'education') {
                       return (
                         <CardResume
-                          key={index}
+                          key={id}
                           title={element.title}
                           year={element.year}
                           society={element.society}
@@ -68,11 +42,47 @@ const Resume = forwardRef(() => {
                     }
                     return null; // Added a return statement for non-matching elements
                   })}
+                </Box>
               </Box>
-            </Box>
+            </Grid>
+
+            {/* WORK TIMELINE SECTION */}
+            <Grid item xs={12} sm={12} md={6} lg={6} xl={6.5}>
+              <Box className={styles.WorkSection}>
+                <Typography className={styles.sectionWorkText} variant="h21">{"Work Timeline"}</Typography>
+                  <Box 
+                    className={styles.resumeTimeline}
+                    sx={{
+                      backgroundColor: theme.palette.mode === 'dark' ? '#d8e5e2' : '#eee3ff',
+                    }}
+                  >
+                    {ResumeData.map((element, index) => {
+                      if (element.category === 'experience') {
+                        return (
+                          <CardResume
+                            key={index}
+                            title={element.title}
+                            year={element.year}
+                            society={element.society}
+                            desc={element.desc}
+                          />
+                        );
+                      }
+                      return null; // Added a return statement for non-matching elements
+                    })}
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+
+        {/*SKILLS SECTION RESUME*/}
+        <Grid container>
+          <Typography className={styles.sectionWorkText} variant="h21">{"Skills"}</Typography>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <SkillsSection />
           </Grid>
         </Grid>
-      </Box>
       </Box>
     </Box>
   );
--- src/components/Home/Resume/Skills/SkillsSection.jsx
+++ src/components/Home/Resume/Skills/SkillsSection.jsx
@@ -1,16 +1,23 @@
-import { Box } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import React from 'react'
 // import styles from './SkillsSection.module.css';
 
 const SkillsSection = () => {
 
   return (
-    <Box 
-      sx={{
-        // borderRadius: '8px',
-        // border: `1.8px solid ${colors.primary[500]}`
-      }}
-    >Section</Box>
+    <Box>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Box>Programming</Box>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Box>Graphic Design</Box>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Box>Others</Box>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
